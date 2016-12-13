92275ab07db51ff0219ba32abc8f2eb35c54d800
Jouie Camille B. Ibarra <jouiecamilleibarra@gmail.com>
2016-12-13 09:12:35 +0800

    Create lab1a.c

---
+++ lab1a.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	
+	int a;
+	int b;
+	int c;
+	int d;
+	
+	printf("enter value of a:\n");
+	scanf("%d", &a);
+	printf("enter value of b:\n");
+	scanf("%d", &b);
+	printf("enter value of c:\n");
+	scanf("%d", &c);
+	
+	
+	printf("average is equals to: %d\n", (a+b+c)/3);
+	
+}
