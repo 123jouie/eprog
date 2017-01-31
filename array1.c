6b4969cbd3c322557aaac44211d7c4950e48fac8
Jouie Camille B. Ibarra <jouiecamilleibarra@gmail.com>
2017-01-31 08:06:27 +0800

    Create array1.c

---
+++ array1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int i, grades[3] , sum=0;
+	for(i=0;i<3; i++){
+		printf("Enter grades %d: ");
+		scanf("%d", &grades[i]);
+	}
+	for(i=0;i<3; i++){
+		sum += grades[1];
+	}
+	average = sum/3;
+	printf("the 2nd grade is: %.2f\n", average);
+	if (average >= 75){
+		printf("pass");
+	}else{
+		printf("fail");
+	}		
+}
